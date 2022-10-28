45250b90cb8969a5193e8bc9b421b31455aa3e34
esrybkin <108972341+esrybkin@users.noreply.github.com>
2022-10-28 21:53:45 +0700

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from game import *
 
 
-app = ApplicationBuilder().token("5321963110:AAF_3G-FZmNSVTUXuvBIfwU4nv9P4IqYJuI").build()
+app = ApplicationBuilder().token("___").build()
 
 app.add_handler(CommandHandler("Help", help_command))
 app.add_handler(CommandHandler("Start", start_command))
